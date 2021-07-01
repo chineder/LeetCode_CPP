766a224b62e684d880a8eee0432b16b0c36d6038
ChiNeDer <chineder@gmail.com>
2021-07-01 19:37:21 +0800

    leecode 13 Signed-off-by: ChiNeDer <chineder@gmail.com>

---
+++ 13. Roman to Integer.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        
+        int num = 0 ;
+        int tmp = 0 ;
+        
+        //玡计>=玡计<
+        //Ωゑ耕ㄢ┮ゑ计材ゎ
+        //程计ぃ﹚琌τぃ⊿Τ计ゑゑ穦out_of_range erroe
+        while(s.size() >= 2)
+        {
+            if(symbolint(s.at(0)) >= symbolint(s.at(1))) num = num + symbolint(s.at(0));
+            else num = num - symbolint(s.at(0));
+            
+            s.erase(0,1);
+        }
+        
+        if(s.size() == 1)
+        {
+            num = num + symbolint(s.at(0));
+        }            
+        return num ;
+    }
+private:
+    int symbolint(char c)
+    {
+        if(c == 'I') return 1 ;
+        else if(c == 'V') return  5;
+        else if(c == 'X') return  10;
+        else if(c == 'L') return  50;
+        else if(c == 'C') return  100;
+        else if(c == 'D') return  500;
+        else if(c == 'M') return  1000;
+        else return 0 ;
+    } 
+    
+};
