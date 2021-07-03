1284c83b2baf47f9da5f794250d4c4f239225323
ChiNeDer <chineder@gmail.com>
2021-07-03 21:25:45 +0800

    leetcode 14

---
+++ 14. Longest Common Prefix.cpp
@@ -0,0 +1,31 @@
+/************************************************************************
+以第一個字串為基準一次一個字元，與後面字的比較
+第二個loop如果遇到不同字元的即通知break，
+
+vector中只有一個字串，那第二個loop，不會進入。
+等放第一個loop跑完，傳回總長度。
+************************************************************************/
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        
+        bool commonPrefix = true ;
+        int length = 0;
+        for(length = 0 ; length < strs[0].size() ; length++)
+        {
+            for(int j = 1 ; j < strs.size() ; j++)
+            {
+                if( strs[0].substr(length,1) != strs[j].substr(length,1))
+                {
+                    commonPrefix = false;
+                    break ;
+                }
+            }
+
+            if(!commonPrefix) break;
+        }
+        
+        return strs[0].substr(0,length) ;
+    }
+};
