1550740ca6f5c63ae6b81e46708e14c527909f40
ChiNeDer <chineder@gmail.com>
2021-07-05 20:30:07 +0800

    leetcode 09

--- 09. Palindrome Number.cpp
+++ 09. Palindrome Number.cpp
@@ -1,3 +1,8 @@
+/************************************************************************************
+Runtime: 20 ms, faster than 21.34% of C++ online submissions for Palindrome Number.
+Memory Usage: 6 MB, less than 27.06% of C++ online submissions for Palindrome Number.
+************************************************************************************/
+
 class Solution {
 public:
     bool isPalindrome(int x) {
@@ -7,15 +12,10 @@ public:
         string s ;
         s = to_string(x) ;
 
-        int i = 0 ,j = s.length()-1 ;       
-             
-        for( ; i <= j ; i++ , j-- )
+        for(int i = 0 ,j = s.length()-1 ; i <= j ; i++ , j-- )
         {   
-            if(s.substr(i,1) != s.substr(j,1)) return false ;
+            if(s.substr(i ,1 ) != s.substr(j ,1)) return false ;
         }
-        
-        
         return true ;
-
     }
 };
