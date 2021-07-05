fffe0b9ef4f7bccd8085243be59e464eb42b6091
ChiNeDer <chineder@gmail.com>
2021-07-05 20:20:35 +0800

    leetcode 14

--- 14. Longest Common Prefix.cpp
+++ 14. Longest Common Prefix.cpp
@@ -4,6 +4,9 @@
 
 vector中只有一個字串，那第二個loop，不會進入。
 等放第一個loop跑完，傳回總長度。
+
+Runtime: 4 ms, faster than 74.03% of C++ online submissions for Longest Common Prefix.
+Memory Usage: 9.1 MB, less than 92.52% of C++ online submissions for Longest Common Prefix.
 ************************************************************************/
 
 class Solution {
@@ -11,6 +14,7 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         
         bool commonPrefix = true ;
+        
         int length = 0;
         for(length = 0 ; length < strs[0].size() ; length++)
         {
